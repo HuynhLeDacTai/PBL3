ce55c09a9cdb1a9dd591e7a1335433cb8ad666f9
AqibChattha <aqibali543@gmail.com>
2022-10-14 20:02:38 +0500

    Add: Translation to English - Update: Form responsiveness
    When working on an open source project you have to use the international language to avoid any communication barriers. A option to change the language of the form DK_Khachhang is introduced and the form is now size responsive.

--- PBL3/DK_Khachhang.Designer.cs
+++ PBL3/DK_Khachhang.Designer.cs
@@ -29,129 +29,228 @@ namespace PBL3
         /// </summary>
         private void InitializeComponent()
         {
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.lb_phoneNumber = new System.Windows.Forms.Label();
+            this.lb_firstLastName = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.tb_firstLastName = new System.Windows.Forms.TextBox();
+            this.tb_phoneNumber = new System.Windows.Forms.TextBox();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.label53 = new System.Windows.Forms.Label();
-            this.button7 = new System.Windows.Forms.Button();
+            this.lb_login = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.languageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vietnameseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.englishToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel4 = new System.Windows.Forms.Panel();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label2
+            // lb_phoneNumber
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.SlateGray;
-            this.label2.Location = new System.Drawing.Point(24, 28);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(115, 20);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Số điện thoại";
+            this.lb_phoneNumber.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lb_phoneNumber.AutoSize = true;
+            this.lb_phoneNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_phoneNumber.ForeColor = System.Drawing.Color.SlateGray;
+            this.lb_phoneNumber.Location = new System.Drawing.Point(3, 10);
+            this.lb_phoneNumber.Name = "lb_phoneNumber";
+            this.lb_phoneNumber.Size = new System.Drawing.Size(115, 20);
+            this.lb_phoneNumber.TabIndex = 1;
+            this.lb_phoneNumber.Text = "Số điện thoại";
             // 
-            // label3
+            // lb_firstLastName
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.SlateGray;
-            this.label3.Location = new System.Drawing.Point(24, 68);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(86, 20);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Họ và tên";
+            this.lb_firstLastName.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lb_firstLastName.AutoSize = true;
+            this.lb_firstLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_firstLastName.ForeColor = System.Drawing.Color.SlateGray;
+            this.lb_firstLastName.Location = new System.Drawing.Point(3, 50);
+            this.lb_firstLastName.Name = "lb_firstLastName";
+            this.lb_firstLastName.Size = new System.Drawing.Size(86, 20);
+            this.lb_firstLastName.TabIndex = 2;
+            this.lb_firstLastName.Text = "Họ và tên";
             // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.button7);
-            this.panel2.Controls.Add(this.textBox2);
-            this.panel2.Controls.Add(this.textBox1);
-            this.panel2.Controls.Add(this.label2);
-            this.panel2.Controls.Add(this.label3);
-            this.panel2.Location = new System.Drawing.Point(3, 54);
+            this.panel2.Controls.Add(this.tableLayoutPanel1);
+            this.panel2.Controls.Add(this.btn_save);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 56);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(402, 178);
+            this.panel2.Size = new System.Drawing.Size(410, 181);
             this.panel2.TabIndex = 18;
             // 
-            // textBox2
+            // btn_save
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(159, 68);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(196, 22);
-            this.textBox2.TabIndex = 4;
+            this.btn_save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_save.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(28)))), ((int)(((byte)(63)))));
+            this.btn_save.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(83)))), ((int)(((byte)(225)))));
+            this.btn_save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_save.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_save.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(141)))), ((int)(((byte)(181)))));
+            this.btn_save.Location = new System.Drawing.Point(301, 143);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(97, 29);
+            this.btn_save.TabIndex = 22;
+            this.btn_save.Text = "Lưu";
+            this.btn_save.UseVisualStyleBackColor = false;
             // 
-            // textBox1
+            // tb_firstLastName
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(159, 26);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(196, 22);
-            this.textBox1.TabIndex = 3;
+            this.tb_firstLastName.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.tb_firstLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_firstLastName.Location = new System.Drawing.Point(168, 49);
+            this.tb_firstLastName.Name = "tb_firstLastName";
+            this.tb_firstLastName.Size = new System.Drawing.Size(208, 22);
+            this.tb_firstLastName.TabIndex = 4;
+            // 
+            // tb_phoneNumber
+            // 
+            this.tb_phoneNumber.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.tb_phoneNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_phoneNumber.Location = new System.Drawing.Point(168, 9);
+            this.tb_phoneNumber.Name = "tb_phoneNumber";
+            this.tb_phoneNumber.Size = new System.Drawing.Size(208, 22);
+            this.tb_phoneNumber.TabIndex = 3;
             // 
             // panel3
             // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(45)))), ((int)(((byte)(86)))));
-            this.panel3.Controls.Add(this.label53);
             this.panel3.Controls.Add(this.panel2);
+            this.panel3.Controls.Add(this.panel4);
             this.panel3.Location = new System.Drawing.Point(12, 12);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(410, 237);
             this.panel3.TabIndex = 19;
             // 
-            // label53
+            // lb_login
             // 
-            this.label53.AutoSize = true;
-            this.label53.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label53.ForeColor = System.Drawing.SystemColors.Info;
-            this.label53.Location = new System.Drawing.Point(28, 11);
-            this.label53.Name = "label53";
-            this.label53.Size = new System.Drawing.Size(354, 29);
-            this.label53.TabIndex = 24;
-            this.label53.Text = "ĐĂNG KÝ KHÁCH HÀNG MỚI";
+            this.lb_login.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lb_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_login.ForeColor = System.Drawing.SystemColors.Info;
+            this.lb_login.Location = new System.Drawing.Point(0, 0);
+            this.lb_login.Name = "lb_login";
+            this.lb_login.Size = new System.Drawing.Size(410, 56);
+            this.lb_login.TabIndex = 24;
+            this.lb_login.Text = "ĐĂNG KÝ KHÁCH HÀNG MỚI";
+            this.lb_login.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // button7
+            // panel1
             // 
-            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(28)))), ((int)(((byte)(63)))));
-            this.button7.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(83)))), ((int)(((byte)(225)))));
-            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button7.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(141)))), ((int)(((byte)(181)))));
-            this.button7.Location = new System.Drawing.Point(152, 112);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(97, 29);
-            this.button7.TabIndex = 22;
-            this.button7.Text = "Lưu";
-            this.button7.UseVisualStyleBackColor = false;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(434, 261);
+            this.panel1.TabIndex = 20;
             // 
-            // Form2
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.languageToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(434, 24);
+            this.menuStrip1.TabIndex = 21;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // languageToolStripMenuItem
+            // 
+            this.languageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.vietnameseToolStripMenuItem,
+            this.englishToolStripMenuItem});
+            this.languageToolStripMenuItem.Name = "languageToolStripMenuItem";
+            this.languageToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.languageToolStripMenuItem.Text = "Language";
+            // 
+            // vietnameseToolStripMenuItem
+            // 
+            this.vietnameseToolStripMenuItem.Name = "vietnameseToolStripMenuItem";
+            this.vietnameseToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.vietnameseToolStripMenuItem.Text = "Vietnamese";
+            this.vietnameseToolStripMenuItem.Click += new System.EventHandler(this.vietnameseToolStripMenuItem_Click);
+            // 
+            // englishToolStripMenuItem
+            // 
+            this.englishToolStripMenuItem.Name = "englishToolStripMenuItem";
+            this.englishToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.englishToolStripMenuItem.Text = "English";
+            this.englishToolStripMenuItem.Click += new System.EventHandler(this.englishToolStripMenuItem_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel1.Controls.Add(this.lb_phoneNumber, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tb_phoneNumber, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tb_firstLastName, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lb_firstLastName, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(11, 38);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(379, 80);
+            this.tableLayoutPanel1.TabIndex = 23;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.lb_login);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(410, 56);
+            this.panel4.TabIndex = 19;
+            // 
+            // DK_Khachhang
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(28)))), ((int)(((byte)(63)))));
-            this.ClientSize = new System.Drawing.Size(434, 261);
-            this.Controls.Add(this.panel3);
-            this.Name = "Form2";
+            this.ClientSize = new System.Drawing.Size(434, 285);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "DK_Khachhang";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form2";
             this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.panel4.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lb_phoneNumber;
+        private System.Windows.Forms.Label lb_firstLastName;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tb_firstLastName;
+        private System.Windows.Forms.TextBox tb_phoneNumber;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Label label53;
-        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label lb_login;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem languageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vietnameseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem englishToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel4;
     }
 }
